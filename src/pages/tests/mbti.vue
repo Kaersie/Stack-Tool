c047beedf6ef81e2ff4c4d99aa73cabd01d0e7aa
kaersie <kaersie@devide>
2024-08-08 23:56:00 +0800

    Thu Aug  8 23:56:00 CST 2024 inscode

--- src/pages/tests/mbti.vue
+++ src/pages/tests/mbti.vue
@@ -7,8 +7,8 @@
         3.请注意,题目的答案无对错之分,你不需要考虑哪个答案“应该”更好,而且不要在任何问题上思考太久,而是应该凭你心里的第一反应做出选择。 <br>
         4.如果你觉得在不同的情境里,两个答案或许都能反映你的倾向,请选择一个对于你的行为方式来说最自然、最顺畅和最从容的答案。<br>
     </p>
-    <el-button style="margin: 30px;" @click="ff" text bg>简易版</el-button>
-    <el-button style="margin: 30px;" text bg>完整版</el-button><br>
+    <el-button style="margin: 30px;" @click="ff2" text bg>简易版</el-button>
+    <el-button style="margin: 30px;" @click="ff1" text bg>完整版</el-button><br>
         <div v-for="item in t">
     <p style="margin: 20px;font-weight: bold;" >{{ item.q }}</p>
     <div style="display: flex;">
@@ -26,12 +26,22 @@
     </div>
 </template>
 <script setup>
-import data from './data/mbti1.json'
+import data1 from './data/mbti1.json'
+import data2 from './data/mbti2.json'
 import { ref } from 'vue'
-data.sort(function(){
+
+const t=ref([{"q":"请先选择", "a":"", "b":"", "r":""}])
+function ff1(){
+    data1.sort(function(){
       return Math.random()-0.5
   })
-const t=ref(data)
+    t.value=data1
+}
+
+function ff2(){
+    t.value=data2
+}
+
 function ff(){
    t.value.forEach(function(item){
     console.log(item.r)
