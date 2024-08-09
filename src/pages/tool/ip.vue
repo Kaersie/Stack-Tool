8a5dccda84c0e83e8fcc208c779792fd4c78119d
kaersie <kaersie@devide>
2024-08-09 23:09:00 +0800

    Fri Aug  9 23:09:00 CST 2024 inscode

--- src/pages/tool/ip.vue
+++ src/pages/tool/ip.vue
@@ -1,18 +1,27 @@
 <template>
-    <el-card>{{ isv }}</el-card>
+    <el-card style="margin:30px">
+      {{ isv }}
+      <h2>您的ip地址<code>{{ ips }}</code></h2>
+    </el-card>
 </template>
 <script setup>
 import  { ref } from 'vue'
 const isv=ref('')
-function getUserIP() {
-      fetch('https://api.ipify.org?format=json')
-        .then(response => response.json())
-        .then(data => {
-          isv.value = data.ip;
-        })
-        .catch(error => {
-          console.log(error);
-        });
-    }
-    getUserIP()
+const ips=ref('')
+var k
+  fetch('https://api.ipify.org?format=json').
+     then(response => response.json()).
+     then(data => {
+     const ip = data.ip
+     //通过ip地址获取所在地
+     fetch(`https://api.vore.top/api/IPdata?ip=${ip}`).
+         then((res) => res.json()
+         .then((data1) => {
+          isv.value=data1;
+          ips.value=JSON.stringify(data1).ipinfo
+         })
+      )
+  })
+  .catch(error => { console.error(error) })
+
 </script>
