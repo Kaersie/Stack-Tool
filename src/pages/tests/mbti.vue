17356ef1041f68b6b167159a277576141e7620d3
kaersie <kaersie@devide>
2024-08-09 14:35:00 +0800

    Fri Aug  9 14:35:00 CST 2024 inscode

--- src/pages/tests/mbti.vue
+++ src/pages/tests/mbti.vue
@@ -36,9 +36,9 @@
   </el-card>
 </template>
 <script setup>
-import data1 from './data/mbti1.json'
-import data2 from './data/mbti2.json'
-import data3 from './data/test.json'
+import data1 from '/data/mbti1.json'
+import data2 from '/data/mbti2.json'
+import data3 from '/data/test.json'
 import { ref } from 'vue'
 const display=ref('off')
 const name=ref("")
