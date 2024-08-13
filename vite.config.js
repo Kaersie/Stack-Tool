c8eb39707275d9b088083d24cd2e88db427b73f7
kaersie <kaersie@devide>
2024-08-13 18:45:00 +0800

    Tue Aug 13 18:45:00 CST 2024 inscode

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [vue(),viteSingleFile(),
+  plugins: [vue(),//viteSingleFile(),
   AutoImport({
     resolvers: [ElementPlusResolver()],
   }),
