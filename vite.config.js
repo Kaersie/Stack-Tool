775ef7c0ec78d151fe9d7ff6e37a5fa437516bef
kaersie <kaersie@devide>
2024-08-13 15:05:00 +0800

    Tue Aug 13 15:05:00 CST 2024 inscode

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
+import { viteSingleFile } from "vite-plugin-singlefile"
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [vue(),
+  plugins: [vue(),viteSingleFile(),
   AutoImport({
     resolvers: [ElementPlusResolver()],
   }),
